2b1f891c043cf3a5f0bb8e098afbd90f248ba4eb
AngelFlores15 <angel.flores@alumnos.uacm.edu.mx>
2025-06-03 19:56:02 -0600

    Add files via upload

---
+++ qwen.js
@@ -0,0 +1,7 @@
+// qwen.js
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const progress = (scrollTop / scrollHeight) * 100;
+  document.querySelector(".scroll-progress").style.width = progress + "%";
+});
